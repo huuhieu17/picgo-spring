c9336f90c918e35f77dab5577a20bc7447c715ee
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2025-09-12 12:42:39 +0700

    fix: mapping data user

--- src/main/java/com/steve/Picgo/mapper/UserMapper.java
+++ src/main/java/com/steve/Picgo/mapper/UserMapper.java
@@ -2,10 +2,17 @@ package com.steve.Picgo.mapper;
 
 import com.steve.Picgo.dtos.auth.RegisterRequest;
 import com.steve.Picgo.entites.UserEntity;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "lastName", source = "lastName")
+    @Mapping(target = "username", source = "username")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "password", source = "password")
     UserEntity toUserEntity(RegisterRequest registerRequest);
 }
